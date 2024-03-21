01dd96c400fb41443662303dc2c0d4b56a2b9e71
beaJunWoo <81508671+enmael@users.noreply.github.com>
2024-03-21 19:47:46 +0900

    dsada
    dsadasd

---
+++ c_c++/c++/day8/Project1/Project1/c_main.cpp
@@ -0,0 +1,39 @@
+/*
+#include <iostream>
+
+class Ipad7
+{
+public:
+	int weight;
+	int length;
+	void Lightning()
+	{
+		std::cout <<c<< "그지 같은 8핀 충전기 또 사야겠네" << std::endl;
+	}
+private:
+	int c = 0;
+};
+class Ipad8 : public Ipad7
+{
+public:
+	void Usbc()
+	{
+		std::cout <<a<<"그지 같은 8핀 충전기 버려도 된다" << std::endl;
+	}
+private:
+	int a = 0;
+};
+int main()
+{
+	Ipad8 a;
+	a.weight = 12;
+	a.length = 11;
+	a.Usbc();
+	a.Lightning();
+
+	Ipad7 d;
+	d.weight = 12;
+	d.length = 11;
+	d.Lightning();
+}
+*/
