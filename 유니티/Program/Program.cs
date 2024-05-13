39c15aa690bfb0403e26c14db8fdbae616b430c4
bsarise <81508671+enmael@users.noreply.github.com>
2024-05-13 14:14:53 +0900

    1일차

---
+++ 유니티/Program/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Program // Note: actual namespace depends on the project name.
+{
+    #region 클래스
+    // 사용자 정의 데이터 유형으로 속성과 함수가
+    // 포함되어 있으며, 클래스를 통해 객체를 새성하여
+    // 접근하고 사용할수 있는 집합체 
+    #endregion
+    internal class Program
+    {
+        // 메인 함수는 프로그램의 진입점 역할을 수행하는 함수 입니다.!
+        static void Main(string[] args)
+        {
+            #region 변수 
+
+
+            #endregion
+            Console.WriteLine("Hello World!");
+            // 명령프로프트 창에서 글자를 출력해주는 함수
+        }
+    }
+}
