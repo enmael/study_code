5cf4d6b33c4286109351e8ee045b2b809c767fb0
bsarise <81508671+enmael@users.noreply.github.com>
2024-05-13 14:48:33 +0900

    1일차
    1일차

--- 유니티/Program/Program.cs
+++ 유니티/Program/Program.cs
@@ -13,11 +13,56 @@ namespace Program // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             #region 변수 
+            // 데이터를 저장할 수 있는 메모리 공간
 
+            // 변수의 메모리 공간은 실행 될때마다 바뀌며 
+            // 여러 개의 변수가 있을때
+            // 서로 고유의 메모리 공간을 가진다
+
+            //메모리 공간으로 해당 메모리에 저장된 값을 참조할 수 있다
+            // 변수를 생성하기 위해 메모리공간의 크기를 지정해주는 자료형 선언
+
+            #region 자료형
+            // 데이터를 저장하기 위해 데이터의 형태를 정해주는 것입니다.
+
+            // 자료형은 각각의 자료형마다 크기가 정해져 있으며,
+            //자료형의 크기는
+            // 바이트 단위로 이루어져 있으며, 자료형의 경우 자료형에 따라 저장할수 있는 값의 종류가 다르다. 
+
+            //자료형 이름    자료형의 크기     값을 저장할 수 있는 범위 
+            //  byte            1byte
+            //  bool            1byte
+            // char             1byte               -128~ 127
+            // short            2byte               -32,768 ~ 32,767
+            // int              4byte               -2147483648 ~ 214748367
+            // float            4byte
+            // double           8byte
+            // decimal          12 byte
 
             #endregion
-            Console.WriteLine("Hello World!");
+
+            #endregion
+            bool flag = true;
+            int data = 100;
+            int number = 12;
+            char name = 'A';
+            float pi = 3.14f;
+            double health = 123.3;
+
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine(number);
+            Console.WriteLine("flag의 값:" + flag);
+            Console.WriteLine("data의 값:" +  data);
+            Console.WriteLine("name의 값:" + name);
+            Console.WriteLine("pi의 값:" + pi);
+            Console.WriteLine("health의 값:" + health);
             // 명령프로프트 창에서 글자를 출력해주는 함수
+            // 배열 없이 학생 프로그램 만들기
+            // 배열 사용해서 학생 프로그램 만들기
+            // 구조체 사용해서 학생 프로그램 만들기
+            // 동적할당 가지고 크기 늘어났다 줄어드는 학생 프로그램 만들기 
+            // 클래스 사용햐소 학생 프로그램 만들기
+
         }
     }
 }
